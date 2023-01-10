50fe61239bb676159b35e8ef2500b47bf75a7fc5
Mikungu <gemik80@gmail.com>
2023-01-10 18:06:25 +0100

    Initial Commit

---
+++ BalunchonGemikMkg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BalunchonGemikMkg
+//
+//  Created by Mikungu Giresse on 10/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
